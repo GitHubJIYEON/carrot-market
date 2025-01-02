a9e0741ea40916a64159b2096adaaca698b5ec5e
GitHubJIYEON <jiyeon2954@gmail.com>
2025-01-02 19:06:46 +0900

    Refactor:  formData, validate  using zod

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,44 @@
 import { z } from "zod";
 
-const usernameSchema = z.string().min(5).max(10);
+const passwordRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/);
+
+//Creating an object schema
+const formSchema = z
+    .object({
+        username: z
+            .string({
+                invalid_type_error: "Username must be a string!",
+                required_error: "Where is my username???",
+            })
+            .min(3, "Way too short!!!")
+            .max(10, "That is too looooong!")
+            .trim()
+            .toLowerCase()
+            .transform((username) => `🔥 ${username}`)
+            .refine((username) => !username.includes("potato"), "No potatoes allowed!"),
+        email: z.string().email().toLowerCase(),
+        password: z
+            .string()
+            .min(4)
+            .regex(
+                passwordRegex,
+                "Passwords must contain at least one UPPERCASE, lowercase, number and special characters #?!@$%^&*-"
+            ),
+        confirm_password: z.string().min(4),
+    })
+    // .refine(checkPasswords, {
+    //     message: "Both passwords should be the same!",
+    //     path: ["confirm_password"],
+    // })
+    .superRefine(({ password, confirm_password }, ctx) => {
+        if (password !== confirm_password) {
+            ctx.addIssue({
+                code: "custom",
+                message: "Two passwords should be equal",
+                path: ["confirm_password"],
+            });
+        }
+    });
 
 export async function createAccount(prevState: any, formData: FormData) {
     const data = {
@@ -9,5 +47,10 @@ export async function createAccount(prevState: any, formData: FormData) {
         password: formData.get("password"),
         confirm_password: formData.get("confirm_password"),
     };
-    usernameSchema.parse(data.username);
+    const result = formSchema.safeParse(data);
+    if (!result.success) {
+        return result.error.flatten();
+    } else {
+        console.log(result.data);
+    }
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,20 @@
 "use client";
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+import FormButton from "@/components/Button";
+import FormInput from "@/components/Input";
 import SocialLogin from "@/components/social-login";
-import { useActionState } from "react";
 import { handleForm } from "./actions";
 
-export default function Login() {
-    const [state, formAction] = useActionState(handleForm, null);
+export default function LogIn() {
+    const [state, action] = useFormState(handleForm, null);
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form action={formAction} className="flex flex-col gap-3">
-                <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
-                <FormInput
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                    errors={state?.error ?? []}
-                />
+            <form action={action} className="flex flex-col gap-3">
+                <FormInput name="email" type="email" placeholder="Email" required />
+                <FormInput name="password" type="password" placeholder="Password" required />
                 <FormButton text="Log in" />
             </form>
             <SocialLogin />
