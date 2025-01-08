356eeb1c5a35af64abfe16bfcb146318798040ea
GitHubJIYEON <jiyeon2954@gmail.com>
2025-01-08 12:49:15 +0900

    Feat: 회원가입 db 일치 확인 여부 코드 추가

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,59 +1,83 @@
-import {
-  PASSWORD_MIN_LENGTH,
-  PASSWORD_REGEX,
-  PASSWORD_REGEX_ERROR,
-} from '@/lib/constants';
-import { z } from 'zod';
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import db from "@/lib/db";
+import { z } from "zod";
+
+const checkUsername = (username: string) => !username.includes("potato");
+const checkUniqueUsername = async (username: string) => {
+    const user = await db.user.findUnique({
+        where: {
+            username,
+        },
+        select: {
+            id: true,
+        },
+    });
+    return !Boolean(user);
+    // if (user) {
+    //     return false;
+    // } else {
+    //     return true;
+    // }
+};
+const checkUniqueEmail = async (email: string) => {
+    const user = await db.user.findUnique({
+        where: {
+            email,
+        },
+        select: {
+            id: true,
+        },
+    });
+    return Boolean(user) === false;
+};
 
 const formSchema = z
-  .object({
-    username: z
-      .string({
-        invalid_type_error: 'Username must be a string!',
-        required_error: 'Where is my username???',
-      })
-      .trim()
-      .toLowerCase()
-      .transform((username) => `🔥 ${username}`)
-      .refine(
-        (username) => !username.includes('potato'),
-        'No potatoes allowed!'
-      ),
-    email: z.string().email().toLowerCase(),
-    password: z
-      .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-    confirm_password: z
-      .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-  })
-  // .refine(checkPasswords, {
-  //     message: "Both passwords should be the same!",
-  //     path: ["confirm_password"],
-  // })
-  .superRefine(({ password, confirm_password }, ctx) => {
-    if (password !== confirm_password) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'Two passwords should be equal',
-        path: ['confirm_password'],
-      });
-    }
-  });
+    .object({
+        username: z
+            .string({
+                invalid_type_error: "Username must be a string!",
+                required_error: "Where is my username???",
+            })
+            .trim()
+            .toLowerCase()
+            // .transform((username) => `${username}`)
+            .refine(checkUsername, "No potatoes allowed!")
+            .refine(checkUniqueUsername, "This username is already taken"),
+        email: z
+            .string()
+            .email()
+            .toLowerCase()
+            .refine(checkUniqueEmail, "There is an account already registered with that email"),
+        password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+        confirm_password: z
+            .string()
+            .min(PASSWORD_MIN_LENGTH)
+            .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    })
+    // .refine(checkPasswords, {
+    //     message: "Both passwords should be the same!",
+    //     path: ["confirm_password"],
+    // })
+    .superRefine(({ password, confirm_password }, ctx) => {
+        if (password !== confirm_password) {
+            ctx.addIssue({
+                code: "custom",
+                message: "Two passwords should be equal",
+                path: ["confirm_password"],
+            });
+        }
+    });
 
 export async function createAccount(prevState: any, formData: FormData) {
-  const data = {
-    username: formData.get('username'),
-    email: formData.get('email'),
-    password: formData.get('password'),
-    confirm_password: formData.get('confirm_password'),
-  };
-  const result = formSchema.safeParse(data);
-  if (!result.success) {
-    return result.error.flatten();
-  } else {
-    console.log(result.data);
-  }
+    const data = {
+        username: formData.get("username"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+        confirm_password: formData.get("confirm_password"),
+    };
+    const result = await formSchema.safeParseAsync(data);
+    if (!result.success) {
+        return result.error.flatten();
+    } else {
+    }
 }
--- lib/db.ts
+++ lib/db.ts
@@ -3,15 +3,15 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function test() {
-    const token = await db.sMSToken.findUnique({
-        where: {
-            id: 1,
-        },
-        include: {
-            user: true,
-        },
-    });
-    console.log(token);
+    try {
+        const token = await db.sMSToken.findUnique({
+            where: { id: 1 },
+            include: { user: true },
+        });
+        console.log(token);
+    } catch (error) {
+        console.error("Error fetching token:", error);
+    }
 }
 test();
 
