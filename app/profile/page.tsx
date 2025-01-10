2c77a3d1697f34cbc907a17dc1e165de65bb8343
GitHubJIYEON <jiyeon2954@gmail.com>
2025-01-10 17:00:48 +0900

    Feat: profile 페이지 - session 확인 후 username 보여주기, 로그아웃 추가

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,40 @@
-export default function Profile() {
-    return <h1>welcome to your profile</h1>;
+//로그인 정보 있는지 확인 (session 확인) -> user 정보 db 가져와 확인
+// O -> 인사말 + 로그아웃 버튼 생성
+// X ->
+
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
+
+async function getUser() {
+    const session = await getSession();
+    if (session.id) {
+        const user = await db.user.findUnique({
+            where: {
+                id: session.id,
+            },
+        });
+        if (user) {
+            return user;
+        }
+        notFound();
+    }
+}
+
+export default async function Profile() {
+    const user = await getUser();
+    const logOut = async () => {
+        "use server";
+        const session = await getSession();
+        session.destroy();
+        redirect("/");
+    };
+    return (
+        <div>
+            <h1>Welcome! {user?.username}!</h1>
+            <form action={logOut}>
+                <button>Log out</button>
+            </form>
+        </div>
+    );
 }
