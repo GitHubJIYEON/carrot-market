cc83728021c3994a9a806c61b7f936aaf328e251
GitHubJIYEON <jiyeon2954@gmail.com>
2025-01-24 23:34:05 +0900

    Feat: github OAuth 추가가

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,35 @@
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest, NextResponse } from 'next/server';
+import getSession from './lib/session';
 
 //인증된 사용자만 접근할 수 있도록 하는 미들웨어 만들기
 
 interface Routes {
-    [key: string]: boolean;
+  [key: string]: boolean;
 }
 
 const publicOnlyUrls: Routes = {
-    "/": true,
-    "/login": true,
-    "/sms": true,
-    "/create-account": true,
+  '/': true,
+  '/login': true,
+  '/sms': true,
+  '/create-account': true,
+  'github/start': true,
+  '/github/complete': true,
 };
 
 export async function middleware(request: NextRequest) {
-    const session = await getSession();
-    const exists = publicOnlyUrls[request.nextUrl.pathname];
-    if (!session.id) {
-        if (!exists) {
-            return NextResponse.redirect(new URL("/", request.url));
-        } else {
-            if (exists) {
-                return NextResponse.redirect(new URL("/products", request.url));
-            }
-        }
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL('/', request.url));
+    } else {
+      if (exists) {
+        return NextResponse.redirect(new URL('/products', request.url));
+      }
     }
+  }
 }
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
